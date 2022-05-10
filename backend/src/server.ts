73bb8328f852e334ea9d043e5aa5a262e7d95b9a
Bruno Cardoso <engenharia4@tkth.com.br>
2022-05-10 11:27:19 -0300

    criacao da rota do create feedbacks

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import { prisma } from './prisma';
 
 const app=express()
+app.use(express.json())
+app.post('/feedbacks',async (req,res)=>{
+    const {type,comment,screenshot}=req.body;
+    const feedback = await prisma.feedback.create({
+        data:{
+            type,
+            comment,
+            screenshot
+        }
+    })
+    res.status(201).json(feedback)
+})
 
-
-
-app.listen(3500,()=>{
-    console.info('Server is ruinnig in port 3500');
+app.listen(3333,()=>{
+    console.info('Server is ruinnig in port 3333');
 })
